bc97e12bb14f69bcdb181a74c4c98e44e035a566
rawlep <rawle.prince@gmail.com>
2015-03-18 16:29:09 +0000

    Update Groups.cs

--- Groups.cs
+++ Groups.cs
@@ -12,61 +12,64 @@ namespace paperTestsCode
         /// <summary>
         /// An enumeration type for comparing values
         /// </summary>
-        public enum Comprison { SrictlyLess, LessOrEqual, StrictlyGreater, GreaterOrEqual };
-        public enum GroupMinimum {One,Two}; 
+        public enum Comprison { SrictlyLess, LessOrEqual, StrictlyGreater, GreaterOrEqual, Proximity };
 
         /// <summary>
-        /// Applying the meta analytic framework
+        /// 
         /// </summary>
-        public Tuple<List<List<List<T>>>, int> metaAnalyticProcedure<T>(Func<T, T, double> function, Comprison comp,
-                                                                       List<T> lst, double tolerance, GroupMinimum group_min)
+        /// <typeparam name="T"> A Type to be instantiated to the elements of the list </typeparam>
+        /// <param name="function">The relationship functions to use to identifying founder sets and defining the "fitness function"</param>
+        /// <param name="comp">An emumeration type specifying the way to compare values when processing the list of potentail combinations</param>
+        /// <param name="lst">The input list of elements of type </param>
+        /// <param name="eta">The tolerance for forming groups.  </param>
+        /// <param name="tau">The least upper bound on the relationship </param>
+        /// <returns> A reduced set of group models for lst</returns>
+        public Tuple<List<List<List<T>>>,int> metaAnalyticProcedure<T>(Func<T, T, double> function, Comprison comp,
+                                                                       List<T> lst, double eta, double tau)
         {
-            // Whether or not we subtract 1 here depends on the optimisation function used.  In our case,
-            // it is justified since we are using the Pearson correlation coefficient and want to prefer those
-            // cases where the correlations are closest to 1.
-            Func<T, T, double> absRelation = (x, y) => Math.Abs(function(x, y) - 1);
+            // 
+            Func<T, T, double> absRelation = (x, y) => (Math.Abs(function(x, y) - tau));
+            Func<List<T>, double> connectedness_aux = ls => apply_to_adjecent_elms(absRelation, ls, 0);
+            Func<List<T>, double> connectedness = ls => (ls == null || ls.Count < 2) ? 2 : connectedness_aux(ls);
+            Func<List<List<T>>, double> modelConnectedness = ls => (ls.Select(x => connectedness(x))).Average();
 
-            // Again whether the sum, average, etc. is used here is dependss on the comparison function
-            Func<List<T>, double> adjacentRelations_aux = ls => (zipp(ls).Select(x => Math.Exp(absRelation(x.Item1, x.Item2)))).Average() ;
-
-            Func<List<T>, double> adjacentRelations = ls => (ls == null || ls.Count < 2) ? 2 : adjacentRelations_aux(ls);
-            Func<List<List<T>>, double> meanAdjacentRelation = ls => (ls.Select(x => adjacentRelations(x))).Average();
-
+  
 
             // ----------------------------------- The method -------------------------------------------------------
             // 1. Get the initial founder sets using the relation function and the stated tolerance
-            List<List<T>> founderSets = getFounderSets(lst, function, tolerance);
-
-
-            // 2. consider ways of recombining the founder sets to form larger groups  
-            var recombinedFounders_aux = allGroups(founderSets, GroupMinimum.One == group_min);
-            List<List<List<T>>> recombinedFounders = recombinedFounders_aux.Select(x => x.Select(y => listConcatenate(y)).ToList()).ToList();
-            int original_recombinations = recombinedFounders.Count;
+            List<List<T>> founderSets = getFounderSets(lst, function, eta);            
             
-            //3. apply a a fitness test to the all the recombined groups using meanAdjacentRelation as a sort of fitness function 
-            var result = maintainBy(meanAdjacentRelation, comp, recombinedFounders);
-
-            // 4. then return those that remained in the queue
-            return Tuple.Create( result, original_recombinations);
+            // 2. consider ways of recombining the founder sets to form larger groups  
+            var recombinedFounders_aux = allGroups(founderSets, true);
+            var recombinedFounders = recombinedFounders_aux.Select(x => x.Select(y => listConcatenate(y)).ToList()).ToList();
+            int original_recombinations = founderSets.Count;
+            
+            // 3. return the reduced set of possibilities
+            var result = maintainBy(modelConnectedness, comp, recombinedFounders);
+            return Tuple.Create(result, original_recombinations); 
         }
 
-        // Form 2-tuples of adjacent elements of a list
-        private List<Tuple<T, T>> zipp<T>(List<T> ls)
+
+        // apply a function to  adjacent elements of the list
+        private double apply_to_adjecent_elms<T>(Func<T,T,double> fun, List<T> list,double dummy)
         {
-            if (ls != null && ls.Count > 1)
-            {
-                var temp = new List<Tuple<T, T>>();
-                for (int i = 0; i < ls.Count - 1; i++)
-                    temp.Add(Tuple.Create(ls.ElementAt(i), ls.ElementAt(i + 1)));
-                return temp;
+            int len = list.Count;
+            var temp = new List<double>();
+            if (len < 2)
+            {   
+                return dummy; 
             }
             else
-                return null;
+            {
+                for (int i = 0; i < len - 1; i++)
+                    temp.Add(fun (list.ElementAt(i),list.ElementAt(i+1)) );
+                return temp.Average();
+            }
         }
 
         /// <summary>
-        /// Applying a fitness function to elements of a list and retain those that rank increasing higher/lower
-        /// subject to a stated comparison 
+        /// Retaining elements in a list that satisfy a given condition such that only those elements of incraesing / decreasing
+        /// subject to some real function on its elements, remains in the list
         /// </summary>
         private List<T> maintainBy<T>(Func<T, double> fn, Comprison comp, List<T> ls)
         {
@@ -83,6 +86,7 @@ namespace paperTestsCode
         //
         private List<T> maintainBy_aux<T>(Func<T, double> fn, T a1, List<T> ls, Comprison comp, List<T> acc)
         {
+            
             if (ls != null && ls.Count > 0)
             {
                 acc = (acc == null) ? new List<T>() : acc;
@@ -90,16 +94,18 @@ namespace paperTestsCode
                 ls.RemoveAt(0);
 
                 // comparing the elements depending on the comparison operator
-                Func<double, double, bool> compare = (a, b) => comp == Comprison.SrictlyLess ? a < b : (comp == Comprison.LessOrEqual ? a <= b : (comp == Comprison.StrictlyGreater ? a > b : a >= b));
+                Func<double, double, bool> compare = (a, b) => comp == Comprison.Proximity ? Math.Abs(b - a) < 0.03 : (comp == Comprison.SrictlyLess ? a < b : (comp == Comprison.LessOrEqual ? a <= b : (comp == Comprison.StrictlyGreater ? a > b : a >= b)));
 
                 if (compare(fn(a1), fn(y)))
                 {
                     prepend_to_list(a1, acc);
+                    //acc.Insert(0,a1);
                     return maintainBy_aux(fn, a1, ls, comp, acc);
                 }
                 else
                 {
                     prepend_to_list(y, acc);
+                    //acc.Insert(0, y);
                     return maintainBy_aux(fn, y, ls, comp, acc);
                 }
             }
@@ -149,7 +155,7 @@ namespace paperTestsCode
             else
             {
                 List<T> front = new List<T>();
-                List<T> back = new List<T>();
+                List<T> back  = new List<T>();
                 for (int i = 0; i < ls.Count; i++)
                 {
                     if (i < n)
@@ -164,7 +170,7 @@ namespace paperTestsCode
         /// <summary> 
         /// All adjacent concatenations,with the option to concatenate at least two elements to form a group
         /// </summary>
-        private List<List<List<T>>> allGroups<T>(List<T> ls, bool one_element_in_a_group)
+        public List<List<List<T>>> allGroups<T>(List<T> ls, bool one_element_in_a_group)
         {
             if (ls != null && ls.Count > 0)
             {
@@ -189,7 +195,7 @@ namespace paperTestsCode
 
         /// <summary>
         /// Calculates all possible adjacent elements from n cuts of the list built from
-        /// at least min_lim elements
+        /// at least min_lim elements, keeping track of the occurrence of cuts
         /// </summary>
         private List<List<List<T>>> nGroups<T>(int m, int min_lim, List<T> inList)
         {
@@ -258,13 +264,13 @@ namespace paperTestsCode
         /// find groups
         private static List<Tuple<List<T>, int>> findGrps_aux1<T>(int n, List<T> list, T elm, Func<T, T, double> fun, double tol, List<Tuple<List<T>, int>> dummy)
         {
-            if (n >= 0 && list != null)
+            if (n >= 0 &&  list != null)
             {
                 if (n > list.Count - 1)
                     return dummy;
                 else
                 {
-                    var front = take(n, list);
+                    var front  = take(n, list);
                     bool valid = front.All(x => fun(elm, x) >= tol);
                     if (valid == false)
                     {
@@ -297,4 +303,5 @@ namespace paperTestsCode
                 return null;
         }
     }
+
 }
